0c44b92b888011d575004d92e93f96244da83b78
Timo Zuccarello <timo@zuccarello.eu>
2023-01-10 18:11:53 +0100

    Use hud_panel_widget from constructor, rather than the inherited member.

--- scripts/5_Mission/gui/ingamehud.c
+++ scripts/5_Mission/gui/ingamehud.c
@@ -3,10 +3,9 @@ modded class IngameHud {
   override void Init(Widget hud_panel_widget) {
     super.Init(hud_panel_widget);
 
-    if (m_HudPanelWidget) {
+    if (hud_panel_widget) {
       // Add our own stuff to the hud.
-      // Class.CastTo(m_RadioPtt, GetGame().GetWorkspace().CreateWidgets("radio_ptt/gui/layouts/radio_ptt_radio.layout", m_HudPanelWidget));
-      Class.CastTo(m_RadioPtt, GetGame().GetWorkspace().CreateWidgets("radio_ptt/gui/layouts/radio_ptt_radio_standalone.layout", m_HudPanelWidget));
+      Class.CastTo(m_RadioPtt, GetGame().GetWorkspace().CreateWidgets("radio_ptt/gui/layouts/radio_ptt_radio_standalone.layout", hud_panel_widget));
     }
   }
 }
